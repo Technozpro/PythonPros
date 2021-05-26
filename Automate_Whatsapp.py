05eb3f0c716d7d0b3b580f54bcfac1f713372255
Prem Gaikwad <premshgaikwad12@gmail.com>
2021-05-26 20:01:30 +0530

    Whatsapp automate

---
+++ Automate_Whatsapp.py
@@ -0,0 +1,2 @@
+import pywhatkit  # import pywhatkit Or Install With Help of  (pip install pywhatkit)
+pywhatkit.sendwhatmsg('+12165846716', "hii", 19, 54)   # Add Number With Country Code Edit Message And Send on Your Needed Time
